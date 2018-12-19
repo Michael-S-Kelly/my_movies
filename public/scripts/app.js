125062af5d4253bf7fb50dabdb635a232fe0ad74
Clarice <claricostello@gmail.com>
2018-12-19 08:52:57 -0800

    toggle details

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,23 @@
-'use strict!';
+'use strict'
+console.log('app.js is running');
 
 //========================Basic Element Declarations=================================
 //------------Elements that will be common between page views------------------------
-const bodyEl = $('#bodyStuff');
-//------------Main Header Elements (Consistant Regardles of View)--------------------
-const headerEl = $(document.createElement('header'));
-//------------Main-Title Elements (Consistant Regardles of View)---------------------
-const mainTitleEl = $(document.createElement('div')).prop('id', 'mainTitle');
-const movieEl = $(document.createElement('h1').radius(384));
-const nightEl = $(document.createElement('h2'));
-const navEl = $(document.createElement('div')); //==Hamburger Menu to Navigate Views=
-const logoEl = $(document.createElement('img'));
-//------------Sub-Title Element (Changes to Match the Page View)---------------------
-const subTitleEl = $(document.createElement('div')).prop('id', 'subTitle');
-//------------Main Element (Changes to Match the Page View)--------------------------
-const mainEl = $(document.createElement('main')).prop('id', 'mainStuff');
-//------------Footer Element (Consistant Regardles of View)--------------------------
-const footerEl = $(document.createElement('footer'));
+// const bodyEl = $('#bodyStuff');
+// //------------Main Header Elements (Consistant Regardles of View)--------------------
+// const headerEl = $(document.createElement('header'));
+// //------------Main-Title Elements (Consistant Regardles of View)---------------------
+// const mainTitleEl = $(document.createElement('div')).prop('id', 'mainTitle');
+// const movieEl = $(document.createElement('h1').radius(384));
+// const nightEl = $(document.createElement('h2'));
+// const navEl = $(document.createElement('div')); //==Hamburger Menu to Navigate Views=
+// const logoEl = $(document.createElement('img'));
+// //------------Sub-Title Element (Changes to Match the Page View)---------------------
+// const subTitleEl = $(document.createElement('div')).prop('id', 'subTitle');
+// //------------Main Element (Changes to Match the Page View)--------------------------
+// const mainEl = $(document.createElement('main')).prop('id', 'mainStuff');
+// //------------Footer Element (Consistant Regardles of View)--------------------------
+// const footerEl = $(document.createElement('footer'));
 //===================================================================================
 
 //=======================Assign Element ID (Used to Get ID in Future Functions)======
@@ -28,31 +29,37 @@ const footerEl = $(document.createElement('footer'));
 //===================================================================================
 
 //=======================Basic Element Relationships=================================
-bodyEl.appendChild(headerEl);
-bodyEl.appendChild(mainEl);
-bodyEl.appendChild(footerEl);
+// bodyEl.appendChild(headerEl);
+// bodyEl.appendChild(mainEl);
+// bodyEl.appendChild(footerEl);
 
-headerEl.appendChild(mainTitleEl);
-headerEl.appendChild(subTitleEl);
+// headerEl.appendChild(mainTitleEl);
+// headerEl.appendChild(subTitleEl);
 
-mainTitleEl.appendChild(movieEl);
-mainTitleEl.appendChild(nightEl);
-mainTitleEl.appendChild(navEl);
-mainTitleEl.appendChild(logoEl);
+// mainTitleEl.appendChild(movieEl);
+// mainTitleEl.appendChild(nightEl);
+// mainTitleEl.appendChild(navEl);
+// mainTitleEl.appendChild(logoEl);
 //===================================================================================
 
 
 // trigger ///
 
-let konamikeys =[];
-let konami = "38,38,40,40,37,39,37,39,66,65";
+// let konamikeys =[];
+// let konami = "38,38,40,40,37,39,37,39,66,65";
 
-$(document).keydown(function(event) {
-  konamikeys.push(event.keycode);
-  if(konamikeys.toString().indexOf(konami) => 0) {
-    $(document).unbind('keydown', arguments.callee);
+// $(document).keydown(function(event) {
+//   konamikeys.push(event.keycode);
+//   if(konamikeys.toString().indexOf(konami) => 0) {
+//     $(document).unbind('keydown', arguments.callee);
 
-    $("body").addClass("konami");
+//     $("body").addClass("konami");
 
-  }
+//   }
+// })
+
+//toggle details
+$('body').on('click', 'img', function(event) {
+  console.log('pop is running')
+  $('.pop-details').toggleClass('pop-hide')
 })
